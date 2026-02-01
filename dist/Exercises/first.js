3b3e2f4bf8f9a501d0214f95e4689df9194c38d4
Misko <m.hresko.dev@gmail.com>
2026-02-01 03:40:01 +0100

    Setting up Typescript

---
+++ dist/Exercises/first.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('This is my first file');
+let age = 20;
+// age = 'a'; 
+// we run program in terminal by tsc name_of_file
+//# sourceMappingURL=first.js.map
---
+++ src/Exercises/first.ts
@@ -0,0 +1,5 @@
+console.log('This is my first file');
+
+let age: number = 20;
+// age = 'a'; 
+// we run program in terminal by tsc name_of_file
